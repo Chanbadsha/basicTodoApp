144edcff09251e1ba27b9f997e2b6d90934b3563
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-07-18 02:49:22 +0600

    create single todo route

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const { URL } = require("url");
 
 // Create path file
 
@@ -8,9 +9,12 @@ const pathFile = path.join(__dirname, "./todos.json");
 
 // Create Todo App Server
 const todoServer = http.createServer((req, res) => {
-  // console.log(req.url)
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathName = url.pathname;
+  // console.log(pathName)
+
   // Get all ToDos
-  if (req.url === "/allTodos" && req.method === "GET") {
+  if (pathName === "/allTodos" && req.method === "GET") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
@@ -18,7 +22,7 @@ const todoServer = http.createServer((req, res) => {
     res.end(data);
   }
   //   Create Todo
-  else if (req.url === "/createTodo" && req.method === "POST") {
+  else if (pathName === "/createTodo" && req.method === "POST") {
     let data = "";
     req.on("data", (chunk) => {
       data = data + chunk;
@@ -26,25 +30,41 @@ const todoServer = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
-      const {id, name, email, age, isActive } = JSON.parse(data);
-const createdAt = new Date().toLocaleString()
-      const newToDo = {id, name, email, age, isActive,createdAt };
+      if (!data) {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify({ error: "Please provide data" }));
+      }
+
+      const { id, name, email, age, isActive } = JSON.parse(data);
+      const createdAt = new Date().toLocaleString();
+      const newToDo = { id, name, email, age, isActive, createdAt };
 
       // get data from db
       const DbData = JSON.parse(
         fs.readFileSync(pathFile, { encoding: "utf-8" })
       );
       DbData.push(newToDo);
-    //   console.log(DbData)
-      const newData = JSON.stringify(DbData,null,2)
+      //   console.log(DbData)
+      const newData = JSON.stringify(DbData, null, 2);
 
-      fs.writeFileSync(pathFile,newData,{encoding:'utf-8'})
-         res.end(newData);
+      fs.writeFileSync(pathFile, newData, { encoding: "utf-8" });
+      res.end(newData);
     });
+  }
 
- 
-  } else {
-    res.end("Welcome to ToDo Server BD");
+  //   Get Single Todo
+  else if (pathName === "/todo" && req.method === "GET") {
+    const id = url.searchParams.get("id");
+    const data = fs.readFileSync(pathFile, { encoding: "utf-8" });
+
+    const parseData = JSON.parse(data);
+    const todos = parseData.find((todo) => todo.id === Number(id));
+
+    res.end(JSON.stringify(todos));
+  }
+  // Update Todo
+ else {
+    res.end("No Route Found");
   }
 });
 
