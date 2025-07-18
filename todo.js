9e4567dd21ebf45fb376cbfb919b9cea25b33e51
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-07-19 00:51:46 +0600

    add new todo

--- todo.js
+++ todo.js
@@ -3,72 +3,69 @@ const path = require("path");
 const fs = require("fs");
 const { URL } = require("url");
 
-// Create path file
+// Database file address
+const filePath = path.join(__dirname, "./todos.json");
 
-const pathFile = path.join(__dirname, "./todos.json");
-
-// Create Todo App Server
+// Create Server
 const todoServer = http.createServer((req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathName = url.pathname;
-  // console.log(pathName)
 
-  // Get all ToDos
-  if (pathName === "/allTodos" && req.method === "GET") {
-    res.writeHead(200, {
-      "content-type": "application/json",
-    });
-    const data = fs.readFileSync(pathFile, { encoding: "utf-8" });
-    res.end(data);
+  // Get All Todo
+  if (pathName === "/todos" && req.method === "GET") {
+    const todos = fs.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(pathName);
+    res.end(todos);
   }
-  //   Create Todo
+  // Get Single Todo
+  else if (pathName === "/todo" && req.method === "GET") {
+    const id = url.searchParams.get("id");
+
+    const todos = JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }));
+    const todo = todos.find((todo) => todo.id === Number(id));
+    res.end(JSON.stringify(todo, null, 2));
+  }
+  //  Create Todo
   else if (pathName === "/createTodo" && req.method === "POST") {
-    let data = "";
+    let todo = "";
+
     req.on("data", (chunk) => {
-      data = data + chunk;
-      // console.log(data)
+      todo += chunk;
     });
 
     req.on("end", () => {
-      if (!data) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ error: "Please provide data" }));
+
+      try {
+        let isDataExist = fs.readFileSync(filePath, "utf-8");
+        let todos = isDataExist.trim() === "" ? [] : JSON.parse(isDataExist);
+        if (!Array.isArray(todos)) {
+              todos = [todos];
+        }
+        console.log(todos)
+        const { name, email, age, isActive } = JSON.parse(todo);
+        const id = todos.length > 0 ? todos[todos.length - 1].id : 0;
+        const newId = id + 1;
+
+        const newTodo = { id: newId, name, email, age, isActive };
+        todos.push(newTodo);
+
+        const updateTodo = JSON.stringify(todos, null, 2);
+        console.log(updateTodo)
+        fs.writeFileSync(filePath, updateTodo, { encoding: "utf-8" });
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "New Todo Created", todo: newTodo }));
+      } catch (err) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        console.log(err)
+        res.end(JSON.stringify({ error: "Failed to create new todo"}));
       }
-
-      const { id, name, email, age, isActive } = JSON.parse(data);
-      const createdAt = new Date().toLocaleString();
-      const newToDo = { id, name, email, age, isActive, createdAt };
-
-      // get data from db
-      const DbData = JSON.parse(
-        fs.readFileSync(pathFile, { encoding: "utf-8" })
-      );
-      DbData.push(newToDo);
-      //   console.log(DbData)
-      const newData = JSON.stringify(DbData, null, 2);
-
-      fs.writeFileSync(pathFile, newData, { encoding: "utf-8" });
-      res.end(newData);
     });
-  }
-
-  //   Get Single Todo
-  else if (pathName === "/todo" && req.method === "GET") {
-    const id = url.searchParams.get("id");
-    const data = fs.readFileSync(pathFile, { encoding: "utf-8" });
-
-    const parseData = JSON.parse(data);
-    const todos = parseData.find((todo) => todo.id === Number(id));
-
-    res.end(JSON.stringify(todos));
-  }
-  // Update Todo
- else {
-    res.end("No Route Found");
+  } else {
+    res.end("No route found");
   }
 });
 
-// Listen ToDo Server BD
 todoServer.listen(5000, () => {
-  console.log(" ✅ Todo Server is running on port 5000");
+  console.log("✅ Todo Server is running on port 5000");
 });
